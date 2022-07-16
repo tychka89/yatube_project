02503f96a5c94ba08b2c7c0e4fba73b6bdb0cb30
Nataliya Rodikova <bachininang@gmail.com>
2022-07-16 21:34:07 +0500

    Словарь контекста, ветвления, циклы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template) 
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'text': 'Это главная страница проекта Yatube',
+    }
+    return render(request, template, context) 
 
 
 def group_posts(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template, context)
